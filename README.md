08140d98bbf2b29ee2b5c3addfa1173f62411ff1
harsharouniyar <harsharouniyar2003@gmail.com>
2026-03-15 13:05:28 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Concrete-Mix-Calculator
+# Concrete Mix Calculator
+
+This project is a simplified concrete mix design calculator.
+
+## Features
+- Predict concrete material quantities
+- Estimate compressive strength
+- Based on research papers and engineering formulas
+
+## Inputs
+- Concrete volume
+- Mix ratio
+- Water cement ratio
+
+## Output
+- Cement bags
+- Sand volume
+- Aggregate volume
+- Water requirement
+
+
---
+++ calculator.py
@@ -0,0 +1,22 @@
+def concrete_mix_calculator(volume, cement_ratio, sand_ratio, aggregate_ratio, wc_ratio):
+
+    dry_volume = volume * 1.54
+
+    total_parts = cement_ratio + sand_ratio + aggregate_ratio
+
+    cement = (cement_ratio / total_parts) * dry_volume
+    sand = (sand_ratio / total_parts) * dry_volume
+    aggregate = (aggregate_ratio / total_parts) * dry_volume
+
+    cement_bags = cement / 0.035
+    cement_weight = cement_bags * 50
+
+    water = wc_ratio * cement_weight
+
+    print("Cement Bags:", round(cement_bags,2))
+    print("Sand Volume:", round(sand,2),"m3")
+    print("Aggregate Volume:", round(aggregate,2),"m3")
+    print("Water Required:", round(water,2),"liters")
+
+volume = float(input("Enter concrete volume (m3): "))
+concrete_mix_calculator(volume,1,2,3,0.5)
